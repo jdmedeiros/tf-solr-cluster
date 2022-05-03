4905f505c12a225995d7e3ebcf031e4d52bc5504
Dan Medeiros <dan.medeiros@sagewell.com>
2022-05-03 19:51:50 +0000

    Configures Solr for SSL

--- prod/services/cloud-config-solr.sh
+++ prod/services/cloud-config-solr.sh
@@ -41,5 +41,9 @@ sed -i 's|#SOLR_SSL_CHECK_PEER_NAME=true|SOLR_SSL_CHECK_PEER_NAME=true|g' /etc/d
 
 scripts/cloud-scripts/zkcli.sh -zkhost "${ZOOKEEPERS}"/solr_v1 -cmd clusterprop -name urlScheme -val https
 
+chmod ugo+x /etc/default/solr.in.sh
 sed -i /etc/default/solr.in.sh -re 's/^#?SOLR_AUTH_TYPE=.*/SOLR_AUTH_TYPE="basic"/; s/^#?SOLR_AUTHENTICATION_OPTS=.*/SOLR_AUTHENTICATION_OPTS="-Dbasicauth=admin:'"${SECRET}"'"/'
+
 service solr restart
+
+/opt/solr/bin/solr auth enable -type basicAuth -prompt true -z "${ZOOKEEPERS}"/solr_v1 -blockUnknown true -credentials admin:"${SECRET}"
